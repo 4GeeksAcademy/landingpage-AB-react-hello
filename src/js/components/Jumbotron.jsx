512bf25b115ef5e8bd92dd1185feeeb98157cc45
andersontbernal <andersonbernal7@gmail.com>
2025-07-13 23:46:55 +0000

    Landing Page

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Jumbotron = () => (
+    <div className="jumbotron bg-light p-5 mt-4 rounded">
+
+        <h1 className="display-4">A Warm Welcome!</h1>
+
+    </div>
+);
+
+export default Jumbotron;
---
+++ src/js/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React  from "react";
+
+const Navbar = () => (
+    <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+            <a className="navbar-brand" href="#">Start Bootstrap</a>
+        </div>
+    </nav>
+);
+
+export default Navbar;
